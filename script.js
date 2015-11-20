3d666afa482afda7b7b3667a00537e287bd92808
Mohamed Ashraf <Eiad@Mohameds-MacBook-Pro.local>
2015-11-20 23:27:54 +0400

    initial push

---
+++ script.js
@@ -0,0 +1,16 @@
+//Dynamic Loop target JS
+
+
+<script type="text/javascript">
+	$(document).ready(function(){
+
+	        $('.order_status_table').click(function(e){
+	            e.preventDefault();
+                    //Take the value of this class attribute "attName1" 
+	            fieldName = $(this).attr('attName1');
+                    // And for this DIV we will search for it's attribute "attName2" and Do whatever
+	            $(' .order-history-container[attName2='+fieldName+'] ').toggleClass('in');
+	        });
+		
+	});
+</script>
